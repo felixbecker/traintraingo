ffa164ed43457624019638fff013403ba17b2f35
Felix from Macbook <mail@felixbecker.net>
2019-05-07 19:54:19 +0200

    changed println from main.go to basic api server

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+)
 
 func main() {
-	fmt.Println("hello train train go!")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "hello train train go!")
+	})
+
+	var gracefulStop = make(chan os.Signal)
+	signal.Notify(gracefulStop, syscall.SIGTERM)
+	signal.Notify(gracefulStop, syscall.SIGINT)
+
+	go func() {
+		sig := <-gracefulStop
+		fmt.Printf("Received signal '%+v' to stop the process gracefully\n", sig)
+		os.Exit(0)
+	}()
+
+	fmt.Println("Started server on localhost:8080. ctl+c to stop")
+	http.ListenAndServe(":8080", nil)
+
 }
